0c64f288078803c949125ab83e711d35fba7dfa7
Koen van Erp <vanerpkoen@gmail.com>
2018-10-30 15:39:11 +0100

    not done yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources tasks only :[:index, :show]
+
+  root "tasks#index"
+
+  #read all
+  get "tasks", to: "tasks#index"
+
+  #read one task
+  get "tasks/:id", to: "tasks#show"
+
+  #create new
+
+  get "tasks/new", to: "tasks#new"
+  post "tasks/create", to: "tasks#create"
+
+  #update
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  #delete task
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
